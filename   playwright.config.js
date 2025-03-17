dc9531d6bd6af24ba0ce663e399f5848cf20e145
Shaileshkumar Bhadani <shaileshkumar.bhadani@smateso.com>
2025-03-17 22:53:57 +0100

    chore: removed report

---   playwright.config.js
+++   playwright.config.js
@@ -1,8 +1,6 @@
 import {defineConfig} from '@playwright/test';
 
 export default defineConfig({
-    reporter: "allure-playwright",
-
     webServer: {
         command: 'npm run dev', port: 3000, url: 'http://localhost:3000',
     }
