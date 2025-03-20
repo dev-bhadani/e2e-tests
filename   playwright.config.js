5bdea9dfe5dcf865b7ae5924db60427d7db38969
Shaileshkumar Bhadani <shaileshkumar.bhadani@smateso.com>
2025-03-20 12:05:08 +0100

    refactor: updated config file

---   playwright.config.js
+++   playwright.config.js
@@ -1,7 +1,15 @@
 import {defineConfig} from '@playwright/test';
 
 export default defineConfig({
-    webServer: {
-        command: 'npm run dev', port: 3000, url: 'http://localhost:3000',
-    }
+    testDir: './tests/specs', timeout: 30000, retries: 2, use: {
+        headless: true, viewport: {width: 1280, height: 720}, actionTimeout: 0, ignoreHTTPSErrors: true,
+    }, webServer: {
+        command: 'npm run dev', port: 3000, url: 'http://localhost:3000', reuseExistingServer: !process.env.CI,
+    }, projects: [{
+        name: 'chromium', use: {browserName: 'chromium'},
+    }, {
+        name: 'firefox', use: {browserName: 'firefox'},
+    }, {
+        name: 'webkit', use: {browserName: 'webkit'},
+    },],
 });
