84e450f77a62bf8306a7dde7020e21253c4af2af
Shaileshkumar Bhadani <shaileshkumar.bhadani@smateso.com>
2025-03-20 12:32:43 +0100

    chore: removed file

--- tests/setup/globalSetup.js
+++
@@ -1,9 +0,0 @@
-import {test} from "@playwright/test";
-
-test.beforeEach(async ({page}) => {
-    page.on('console', message => {
-        if (message.type() === 'error') {
-            console.error(`Frontend Error: ${message.text()}`);
-        }
-    });
-});
